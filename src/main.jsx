05653807a08b8e769dbbde969cc8bc482efab362
ConnerKrenzer <149502901+ckrenzer720@users.noreply.github.com>
2025-04-01 11:16:43 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,14 @@ import { BrowserRouter } from 'react-router-dom';
 const AUTH_DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN;
 const AUTH_CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
+/**
+ * TODO: Ticket 3:
+ * Implement authentication using Auth0:
+ * - Wrap ProvideAppContext with the Auth Provider from Auth0
+ * - Add your credentials from Auth0 to a .env file (AUTH_DOMAIN, AUTH_CLIENT_ID)
+ * - Set the domain, clientId, and authorizationParams
+ */
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Auth0ProviderWithConfig>
