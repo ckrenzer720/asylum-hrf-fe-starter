d556137a282260c86ca175ca0cc14162a84d63e3
ConnerKrenzer <149502901+ckrenzer720@users.noreply.github.com>
2025-04-01 11:14:44 -0400

    Update LoggingButtons.jsx

--- src/auth/LoggingButtons.jsx
+++ src/auth/LoggingButtons.jsx
@@ -1,5 +1,10 @@
 import { useAuth0 } from '@auth0/auth0-react';
 
+/*
+ * TODO: Ticket 3:
+ * Implement authentication and logging functionality using Auth0
+*/
+
 export const LoggingButtons = () => {
   // Auth0 functionality
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
